d76c4289eab1c475270f1de0808ac8aced1030a5
cicerogusta <ciceroandrade2525@gmail.com>
2021-12-08 00:33:57 -0300

    layout do formulário

--- lib/form/formulario.dart
+++ lib/form/formulario.dart
@@ -5,8 +5,36 @@ class FormularioTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Text("Formulário"),
+        appBar: AppBar(
+          title: Text("Criando Transferência"),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                style: TextStyle(fontSize: 16),
+                decoration: InputDecoration(
+                    labelText: 'Número da Conta', hintText: '00000'),
+              ),
+            ),Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                style: TextStyle(fontSize: 16),
+                decoration: InputDecoration(
+                    labelText: 'Valor', hintText: '0.00', icon: Icon(Icons.monetization_on)),
+              ),
+            ),
+
+            ElevatedButton(onPressed: () {
+              print('Clicou');
+            }, child: Text("Confirmar"),)
+          ],
+        ),
       ),
     );
   }
